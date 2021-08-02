3f9d66f6f34a94437d2edf62f6b1924f3045ad66
Islanderrobotics <75331800+Islanderrobotics@users.noreply.github.com>
2021-08-02 19:51:17 -0400

    looking good

--- database.py
+++ database.py
@@ -1,17 +1,23 @@
 import os
 import zipfile
 import pandas as pd
+from datetime import date
 class Database:
-    def __init__(self,API_call, removefile = False):
-        self.name = API_call.split("/")
+    def __init__(self,API_call):
         os.system(API_call)
-        with zipfile.ZipFile(self.name[1]+".zip", 'r') as zip:
-            self.filename = zip.namelist()
-            zip.extractall()
+        self.name = API_call.split("/")
+        self.Open()
+        os.remove(self.name[1]+".zip")
+    def Open(self):
         self.dataset = []
-        for i in self.filename:
+        with zipfile.ZipFile(self.name[1]+".zip", 'r') as zip:
+            filename = zip.namelist()
+            zip.extractall()
+        newfilename = []
+        for i in filename:
+            name = i.split(".")[0]
+            os.rename(rf"{i}", rf"{name}-{date.today()}.csv")
+            newfilename.append(name+"-"+str(date.today())+".csv")
+        for i in newfilename:
             self.dataset.append(pd.read_csv(i))
-        if(removefile):
-            os.remove(self.name[1]+".zip")
-            for i in self.filename:
-                os.remove(i)
+        self.filenames=newfilename
